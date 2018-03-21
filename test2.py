9e740038e974179d29529efacdb7e440a9378900
somecat1996 <369150573@qq.com>
2018-03-21 13:25:45 +0800

    adjustment

--- test2.py
+++ test2.py
@@ -2,22 +2,28 @@ from scapy.all import *
 from chardet import detect
 from scapy.layers import http
 
-buffer = PcapReader("wanyong-80.pcap")
-while True:
-    packege = buffer.read_packet()
-    if packege is None:
-        break
-    else:
-        try:
-            tmp = packege[TCP]
-            if tmp.dport == 80 or tmp.sport == 80:
-                tmp = tmp.payload
-                print(tmp.Method)
-                print(tmp.Host)
-                if tmp.Referer != '':
-                    print(tmp.Referer)
-                else:
-                    print('None')
-                print()
-        except:
-            pass
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('usage:python test filepath/filename')
+        exit(1)
+    buffer = PcapReader(sys.argv[1])
+    while True:
+        packege = buffer.read_packet()
+        if packege is None:
+            break
+        else:
+            try:
+                tmp = packege[TCP]
+                if tmp.dport == 80 or tmp.sport == 80:
+                    tmp = tmp.payload
+                    print(tmp.Method)
+                    print(tmp.Host)
+                    if tmp.Referer != '':
+                        print(tmp.Referer)
+                    else:
+                        print('None')
+                    print()
+            except:
+                pass
+
+
