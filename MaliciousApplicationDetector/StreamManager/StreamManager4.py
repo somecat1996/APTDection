ab60ec8fada80daa771568d17bd66ea9c45eddaa
somecat1996 <369150573@qq.com>
2018-05-19 21:18:37 +0800

    .

--- MaliciousApplicationDetector/StreamManager/StreamManager4.py
+++ MaliciousApplicationDetector/StreamManager/StreamManager4.py
@@ -4,28 +4,27 @@ from scapy.all import *
 from DataLabeler.DataLabeler import Datalabler
 
 class StreamManager:
-    def __init__(self,filename):
-        self.filename=filename
-        path=os.getcwd()
-        self.datapath=path+"/stream/"+filename.strip('.pcap')
-        self.browser_groups_PC={}
-        self.backgroud_groups_PC={}
+    def __init__(self, filename):
+        self.filename = filename
+        path = os.getcwd()
+        self.datapath = path + "/stream/" + filename.strip('.pcap')
+        self.browser_groups_PC = {}
+        self.backgroud_groups_PC = {}
         self.browser_groups_Phone = {}
         self.backgroud_groups_Phone = {}
-        self.suspicious_group={}
-        self.browser_PC=[]
-        self.backgroud_PC=[]
+        self.suspicious_group = {}
+        self.browser_PC = []
+        self.backgroud_PC = []
         self.browser_Phone = []
         self.backgroud_Phone = []
-        self.suspicious=[]
-
+        self.suspicious = []
 
     def generate(self):
-        back=os.path.dirname(os.path.realpath(__file__))
+        back = os.path.dirname(os.path.realpath(__file__))
         os.chdir(self.datapath)
-        re=os.system("pkt2flow -xv -o ./tmp "+self.filename)
+        re = os.system("pkt2flow -xv -o ./tmp " + self.filename)
         print("执行命令")
-        if re!=0:
+        if re != 0:
             print("流转化失败！")
             return
         os.chdir("./tmp")
@@ -35,37 +34,37 @@ class StreamManager:
         os.system("rm -r tcp_syn/")
         os.chdir(back)
 
-    def classify(self,ips):
-        files = os.listdir(self.datapath+"/tmp")
-        count_f=0
-        count_no=0
-        total=len(files)
+    def classify(self, ips):
+        files = os.listdir(self.datapath + "/tmp")
+        count_f = 0
+        count_no = 0
+        total = len(files)
         for x in files:
-            ip=self.getIP(x)
+            ip = self.getIP(x)
             if ip in ips[-1]:
-                print("过滤流文件:",x)
+                print("过滤流文件:", x)
                 print(len(files))
-                count_f+=1
+                count_f += 1
                 continue
             if ip in ips[0]:
-                self.browser_PC.append({"filename":x,"type":1,"is_malicious":0})
+                self.browser_PC.append({"filename": x, "type": 1, "is_malicious": 0})
             elif ip in ips[1]:
-                self.backgroud_PC.append({"filename":x,"type":2,"is_malicious":0})
+                self.backgroud_PC.append({"filename": x, "type": 2, "is_malicious": 0})
             elif ip in ips[2]:
-                self.browser_Phone.append({"filename":x,"type":3,"is_malicious":0})
+                self.browser_Phone.append({"filename": x, "type": 3, "is_malicious": 0})
             elif ip in ips[3]:
-                self.backgroud_Phone.append({"filename":x,"type":4,"is_malicious":0})
+                self.backgroud_Phone.append({"filename": x, "type": 4, "is_malicious": 0})
             elif ip in ips[4]:
-                self.suspicious.append({"filename":x,"type":5,"is_malicious":0})
+                self.suspicious.append({"filename": x, "type": 5, "is_malicious": 0})
             else:
-                print("找不到",ip)
-                count_no+=1
+                print("找不到", ip)
+                count_no += 1
 
-        print("共有:",total,"个文件")
-        print("过滤了：",count_f,"个文件")
-        print("找不到",count_no,"个文件")
+        print("共有:", total, "个文件")
+        print("过滤了：", count_f, "个文件")
+        print("找不到", count_no, "个文件")
 
-        print("PC浏览器：",self.browser_PC)
+        print("PC浏览器：", self.browser_PC)
         print("-----------------------------")
         print("PC软件：", self.backgroud_PC)
         print("-----------------------------")
@@ -73,11 +72,10 @@ class StreamManager:
         print("-----------------------------")
         print("Phone软件：", self.backgroud_Phone)
         print("-----------------------------")
-        print("无ua嫌疑软件：",self.suspicious)
-
+        print("无ua嫌疑软件：", self.suspicious)
 
     def Group(self):
-        #labling
+        # labling
         '''
         print("开始标记数据")
         print("正在标记数据类型1...")
@@ -93,24 +91,23 @@ class StreamManager:
         print("数据标记完毕，开始聚类...")
         '''
 
-        #grouping
-        #browsertype
+        # grouping
+        # browsertype
         for i in range(len(self.browser_PC)):
             UA = self.getUA(self.browser_PC[i]["filename"])
             ip = self.getIP(self.browser_PC[i]["filename"])
-            if ip==0:
+            if ip == 0:
                 continue
-            tag=ip[0]
+            tag = ip[0]
             if self.isLocalIP(ip[0]):
-                tag=ip[1]
+                tag = ip[1]
             key = tag + " " + UA
             if key in self.browser_groups_PC:
                 self.browser_groups_PC[key].append(self.browser_PC[i])
             else:
-                tmp=[]
+                tmp = []
                 tmp.append(self.browser_PC[i])
-                self.browser_groups_PC[key]=list(tmp)
-
+                self.browser_groups_PC[key] = list(tmp)
 
         # backgroudtype PC
         for i in range(len(self.backgroud_PC)):
@@ -168,7 +165,7 @@ class StreamManager:
             tag = ip[0]
             if self.isLocalIP(ip[0]):
                 tag = ip[1]
-            key = tag + " "+UA
+            key = tag + " " + UA
             if key in self.suspicious_group:
                 self.suspicious_group[key].append(self.suspicious[i])
             else:
@@ -196,7 +193,7 @@ class StreamManager:
         print("正在标记数据类型5...")
         self.lable(self.suspicious_group)
        '''
-        #validating
+        # validating
 
     def labelGroups(self):
         print("开始标记数据")
@@ -211,11 +208,11 @@ class StreamManager:
         print("正在标记数据类型5...")
         self.lable(self.suspicious_group)
 
-    def lable(self,target_groups):
-        urls=[]
-        keys=[]
+    def lable(self, target_groups):
+        urls = []
+        keys = []
         for key in target_groups:
-            ip=self.extractIP(key)
+            ip = self.extractIP(key)
             urls.append(ip)
             keys.append(key)
         print(urls)
@@ -229,35 +226,34 @@ class StreamManager:
         if not urls:
             print("无内容需要标记")
             return
-        l=Datalabler()
+        l = Datalabler()
         l.setThreadNum(20)
         result = l.lable(urls)
         for x in result:
-            url_tmp=x["url"]
+            url_tmp = x["url"]
             for i in range(len(urls)):
                 if url_tmp == urls[i]:
                     for index in range(len(target_groups[keys[i]])):
-                        target_groups[keys[i]][index]["is_malicious"]+=x["malicious"]
-                        target_groups[keys[i]][index]["is_malicious"] +=x["suspicious"]
-                        if x["malicious"]!=0 or x["suspicious"]!=0:
+                        target_groups[keys[i]][index]["is_malicious"] += x["malicious"]
+                        target_groups[keys[i]][index]["is_malicious"] += x["suspicious"]
+                        if x["malicious"] != 0 or x["suspicious"] != 0:
                             print("扫描命中！！！！")
 
-
-    def validate(self,dict):
-        targets=[]
+    def validate(self, dict):
+        targets = []
         for key in dict:
             for x in dict[key]:
-                if x["is_malicious"]>0:
+                if x["is_malicious"] > 0:
                     targets.append(x)
-        urls=[]
-        index=[]
+        urls = []
+        index = []
 
-        malicious_num=0
+        malicious_num = 0
         for i in range(len(targets)):
-            filename=targets[i]["filename"]
-            url=self.GetUrl(filename)
-            if url=="none":
-                malicious_num+=1
+            filename = targets[i]["filename"]
+            url = self.GetUrl(filename)
+            if url == "none":
+                malicious_num += 1
             else:
                 urls.append(url)
                 index.append(i)
@@ -266,33 +262,33 @@ class StreamManager:
             print("无内容需要验证")
             return []
         else:
-            url_to_scan=list(set(urls))
+            url_to_scan = list(set(urls))
 
-        false_alarm=[]
+        false_alarm = []
 
         l = Datalabler()
         l.setThreadNum(20)
         result = l.lable(url_to_scan)
         for x in result:
-            if x["malicious"]==0 and x["suspicious"]==0:
-                url_tmp=x["url"]
+            if x["malicious"] == 0 and x["suspicious"] == 0:
+                url_tmp = x["url"]
                 for i in range(len(urls)):
-                    if url_tmp==urls[i]:
+                    if url_tmp == urls[i]:
                         false_alarm.append(targets[index[i]]["filename"])
             else:
-                malicious_num+=1
+                malicious_num += 1
 
-        print("总共标记:",len(targets),"个恶意流")
-        print("virustotal检测出的恶意流个数为:",malicious_num)
+        print("总共标记:", len(targets), "个恶意流")
+        print("virustotal检测出的恶意流个数为:", malicious_num)
 
         return false_alarm
 
-    def extractIP(self,ipUA):
-        raw=ipUA.split()
+    def extractIP(self, ipUA):
+        raw = ipUA.split()
         return raw[0]
 
     def GetDataForCNN(self):
-        tmp=[]
+        tmp = []
         for key in self.browser_groups_PC:
             for x in self.browser_groups_PC[key]:
                 tmp.append(x)
@@ -300,7 +296,6 @@ class StreamManager:
         for key in self.backgroud_groups_PC:
             for x in self.backgroud_groups_PC[key]:
                 tmp.append(x)
-
 
         for key in self.browser_groups_Phone:
             for x in self.browser_groups_Phone[key]:
@@ -336,21 +331,21 @@ class StreamManager:
         print(self.suspicious_group)
         return self.suspicious_group
 
-    def getIP(self,filename):
-        tmp=filename.split("_")
-        ip1=tmp[0]
-        ip2=tmp[2]
-        result=[]
+    def getIP(self, filename):
+        tmp = filename.split("_")
+        ip1 = tmp[0]
+        ip2 = tmp[2]
+        result = []
         result.append(ip1)
         result.append(ip2)
         result.sort()
         return result
 
-    def getUA(self,filename):
-        filepath = self.datapath + "/tmp/"+filename
-        pattern="User-Agent.*?\\\\r"
+    def getUA(self, filename):
+        filepath = self.datapath + "/tmp/" + filename
+        pattern = "User-Agent.*?\\\\r"
 
-        useragent="UnknownUA"
+        useragent = "UnknownUA"
 
         source = PcapReader(filepath)
         packet = source.read_packet()
@@ -360,12 +355,12 @@ class StreamManager:
             except:
                 packet = source.read_packet()
                 continue
-            ptr=".*(GET|POST|HEAD).*HTTP.*"
+            ptr = ".*(GET|POST|HEAD).*HTTP.*"
             if re.match(ptr, s):
                 try:
-                    ua = re.findall(pattern,s)[0].strip("\\r")
-                    ua=re.sub("User-Agent: ","",ua)
-                    useragent=ua
+                    ua = re.findall(pattern, s)[0].strip("\\r")
+                    ua = re.sub("User-Agent: ", "", ua)
+                    useragent = ua
                 except:
                     packet = source.read_packet()
                     continue
@@ -373,15 +368,15 @@ class StreamManager:
             packet = source.read_packet()
         return useragent
 
-    def GetUrl(self,filename):
-        filepath=self.datapath+"/tmp/"+filename
-        pattern1= "/.*?HTTP"
-        pattern2="/.*?\\?"
-        pattern3="Host.*?\\\\r"
+    def GetUrl(self, filename):
+        filepath = self.datapath + "/tmp/" + filename
+        pattern1 = "/.*?HTTP"
+        pattern2 = "/.*?\\?"
+        pattern3 = "Host.*?\\\\r"
 
         source = PcapReader(filepath)
         packet = source.read_packet()
-        uri_tmp="none"
+        uri_tmp = "none"
         while packet:
             try:
                 s = str(packet[Raw].load)
@@ -400,18 +395,18 @@ class StreamManager:
                 except:
                     packet = source.read_packet()
                     continue
-                uri_tmp=re.sub("http://","",uri)
+                uri_tmp = re.sub("http://", "", uri)
                 break
             else:
                 packet = source.read_packet()
 
         return uri_tmp
 
-    def isLocalIP(self,IP):
-        local=["10\\..*","192\\.168\\..*","172\\.16\\..*","172\\.17\\..*","172\\.18\\..*","172\\.19\\..*"
-            , "172\\.20\\..*","172\\.21\\..*","172\\.22\\..*","172\\.23\\..*","172\\.24\\..*","172\\.25\\..*"
-            , "172\\.26\\..*","172\\.27\\..*","172\\.28\\..*","172\\.29\\..*","172\\.30\\..*","172\\.31\\..*"]
+    def isLocalIP(self, IP):
+        local = ["10\\..*", "192\\.168\\..*", "172\\.16\\..*", "172\\.17\\..*", "172\\.18\\..*", "172\\.19\\..*"
+            , "172\\.20\\..*", "172\\.21\\..*", "172\\.22\\..*", "172\\.23\\..*", "172\\.24\\..*", "172\\.25\\..*"
+            , "172\\.26\\..*", "172\\.27\\..*", "172\\.28\\..*", "172\\.29\\..*", "172\\.30\\..*", "172\\.31\\..*"]
         for x in local:
-            if re.match(x,IP):
+            if re.match(x, IP):
                 return True
         return False
