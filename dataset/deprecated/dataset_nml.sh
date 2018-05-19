d8453f9c536741cf29b7a45f627ecaf18980f43d
Jarry Shaw <JarryShaw@users.noreply.github.com>
2018-05-19 23:08:44 +0800

    .

--- dataset/deprecated/dataset_nml.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-counts=`python3 -c "for index in __import__('random').choices(range(0, 39), k=10): print(str(index).rjust(3, '0'))"`
-for index in $counts; do
-    nohup python3 dataset.py "wanyong80_${index}.pcap" >> "nohup_${index}.out" &
-done
