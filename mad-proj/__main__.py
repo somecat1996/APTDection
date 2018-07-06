495448b58f0997506d12c4ab0bf5bf6078613805
Jarry Shaw <JarryShaw@users.noreply.github.com>
2018-07-06 09:35:48 +0800

    Update mad-proj.

--- mad-proj/__main__.py
+++ mad-proj/__main__.py
@@ -1,26 +1,44 @@
 # -*- coding: utf-8 -*-
 
 
+import collections
 import multiprocessing
 import signal
+import subprocess
 
 import pcapkit
-import scapy.all as scapy
+import pcapkit.all
+import scapy.all
 
 
 IFACE = 'eth0'
 TIMEOUT = 1000
+RETRAIN = False
 
 
 def main(*, iface=None):
     """Main interface for MAD."""
     signal.signal(signal.SIGUSR1, make_worker)
+    signal.signal(signal.SIGUSR2, retrain_cnn)
 
     if iface is not None:
         global IFACE
         IFACE = iface
 
     make_worker()
+
+
+def retrain_cnn():
+    """Run retrain process."""
+    # if already under retrain do nothing
+    if RETRAIN:     return
+
+    # update retrain flag
+    global RETRAIN
+    RETRAIN = True
+
+    # start retrain
+    run_retrain(...)
 
 
 def make_worker():
@@ -33,11 +51,28 @@ def start_worker():
     """Start child process."""
     # first, we sniff packets using Scapy
     # set time limit and interface
-    scapy.sniff(timeout=TIMEOUT, iface=IFACE)
+    sniffed = scapy.all.sniff(timeout=TIMEOUT, iface=IFACE)
+
+    # now, we send a signal to the parent process
+    # to create a new process and continue
+    os.kill(os.getppid(), signal.SIGUSR1)
 
     # then, we trace and make index for TCP flow of packets sniffed
     # using PyPCAPKit, whose interface is to be done by tmrw
-    index = pcapkit.trace(...)
+    traceflow = pcapkit.trace()
+    for count, packet in enumerate(sniffed):
+        flag, data = pcapkit.scapy_tcp_traceflow(packet, count=count)
+        if flag:    traceflow(data)
+    traceindex = traceflow.index
+
+    index = list()
+    for flow in traceindex:
+        templist = list()
+        for number in flow.index:
+            analysis = pcapkit.analyse(file=bytes(sniffed[number]['TCP'].payload))
+            if pcapkit.protocols.application.httpv1.HTTPv1 in analysis.protochain:
+                templist.append(analysis.info.get('header', dict()).get('User-Agent', 'UnknownUA'))
+        index.append(pcapkit.all.Info(flow, ua=collections.Counter(templist).most_common(1)[0][0]))
 
     # afterwards, we will work on each flow
     for flow in index:
