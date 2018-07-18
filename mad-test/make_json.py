137a7680fbe5adfd67874240495308554f0340e8
Jarry Shaw <jarryshaw@icloud.com>
2018-07-19 00:56:27 +0800

    dpkt bug

--- mad-test/make_json.py
+++ mad-test/make_json.py
@@ -4,15 +4,15 @@ import json
 import os
 
 temp = list()
-temp.extend(map(lambda name: f'/data/wanyong-httpdump/20180407/{name}',
-                filter(lambda name: '.pcap' in os.path.splitext(name)[1],
-                        os.listdir(f"/data/wanyong-httpdump/20180407"))))
-temp.extend(map(lambda name: f'/data/wanyong-httpdump/20180408/{name}',
-                filter(lambda name: '.pcap' in os.path.splitext(name)[1],
-                        os.listdir(f"/data/wanyong-httpdump/20180408"))))
 temp.extend(map(lambda name: f'/data/wanyong-httpdump/20180408/20180309/{name}',
                 filter(lambda name: '.pcap' in os.path.splitext(name)[1],
-                        os.listdir(f"/data/wanyong-httpdump/20180408/20180309"))))
+                        sorted(os.listdir(f"/data/wanyong-httpdump/20180408/20180309")))))
+temp.extend(map(lambda name: f'/data/wanyong-httpdump/20180407/{name}',
+                filter(lambda name: '.pcap' in os.path.splitext(name)[1],
+                        sorted(os.listdir(f"/data/wanyong-httpdump/20180407")))))
+temp.extend(map(lambda name: f'/data/wanyong-httpdump/20180408/{name}',
+                filter(lambda name: '.pcap' in os.path.splitext(name)[1],
+                        sorted(os.listdir(f"/data/wanyong-httpdump/20180408")))))
 
 with open('./data.json', 'w') as file:
     json.dump(temp, file)
