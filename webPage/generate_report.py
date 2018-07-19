5bf21174e72e4a44ec91765321bc66411a9a4b72
somecat1996 <369150573@qq.com>
2018-07-19 14:59:40 +0800

    .

--- webPage/generate_report.py
+++ webPage/generate_report.py
@@ -8,7 +8,7 @@ def readReportList(path):
         index = json.load(f)
     return index
 
-def writeInnerIP(index):
+def writeUA(index):
     UA = list()
     if os.path.isfile("UA.json"):
         with open("UA.json", 'r') as f:
@@ -22,6 +22,70 @@ def writeInnerIP(index):
             info = i['info']
             src = i['srcIP']
             dst = i['dstIP']
+            flag = True
+            for j in UA:
+                if name == j['name'] and type == j['type']:
+                    flag = False
+                    if time < j['stime']:
+                        j['stime'] = time
+                    elif time > j['etime']:
+                        j['etime'] = time
+                    j['src'].append(src)
+                    j['dst'].append(dst)
+                    break
+            if flag:
+                UA.append({
+                    "name": name,
+                    "type": type,
+                    "stime": time,
+                    "etime": time,
+                    "info": info,
+                    "src": [src],
+                    "dst": [dst]
+                })
+    with open("UA.json", 'w') as f:
+        json.dump(f, UA)
+
+def writeInnerIP(index):
+    innerIP = list()
+    if os.path.isfile("innerIP.json"):
+        with open("innerIP.json", 'r') as f:
+            innerIP.extend(json.load(f))
+    for file in index:
+        tmp_data = json.load(open(file, 'r'))
+        for i in tmp_data:
+            name = i['UA']
+            type = i['is_malicious']
+            time = dt.datetime.fromisoformat(i['time'])
+            info = i['info']
+            src = i['srcIP']
+            dst = i['dstIP']
+            flag = True
+            for j in UA:
+                if name == j['name'] and type == j['type']:
+                    flag = False
+                    if time < j['stime']:
+                        j['stime'] = time
+                    elif time > j['etime']:
+                        j['etime'] = time
+                    j['src'].append(src)
+                    j['dst'].append(dst)
+                    break
+            if flag:
+                UA.append({
+                    "name": name,
+                    "type": type,
+                    "stime": time,
+                    "etime": time,
+                    "info": info,
+                    "src": [src],
+                    "dst": [dst]
+                })
+    with open("innerIP.json", 'w') as f:
+        json.dump(f, innerIP)
 
 if __name__ == "__main__":
-    pass
+    index = readReportList("./report")
+    writeInnerIP(index)
+    writeUA(index)
+
