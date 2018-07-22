86d548d783b0c4ddf7ccd9172eae84e0d69e010e
Jarry Shaw <jarryshaw@icloud.com>
2018-07-22 13:36:09 +0800

    Regular update

--- webPage/geoip.py
+++ webPage/geoip.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 
+import copy
+import ipaddress
 import json
 import os
 
@@ -10,18 +12,47 @@ import geocoder
 with open('/usr/local/mad/report/Background_PC/index.json') as file:
     filelist = json.load(file)
 
-geoip = list()
-for filename in filelist:
-    with open(filename) as file:
+ipset = list()
+for filename in sorted(filelist):
+    # print(f'/usr/loca/mad{filename}') ###
+    with open(f'/usr/local/mad{filename}') as file:
         report = json.load(file)
     for item in report:
         if not item['is_malicious']:    continue
-        ip = item['dstIP']
-        latlng = geocoder.ip(ip).latlng
+        ipset.append(item['dstIP'])
+print(list(sorted(set(ipset)))) ###
+resip = list()
+geoip = list()
+for count, ip in enumerate(sorted(set(ipset))):
+    if ipaddress.ip_address(ip).is_private:
+        print(count+1, ip, 'private address')
+        continue
+    latlng = geocoder.ip(ip).latlng
+    print(count+1, ip, latlng) ###
+    if latlng:
         geoip.append(dict(
             name=ip,
             latLng=latlng,
         ))
+    if latlng is None:
+        resip.append((ip, 0))
+
+while resip:
+    temp = copy.deepcopy(resip)
+    resip = list()
+    for ip, count in temp:
+        if count > 100:
+            print('failed', ip, count)
+        count += 1
+        latlng = geocoder.ip(ip).latlng
+        print('retry', ip, latlng) ###
+        if latlng:
+            geoip.append(dict(
+                name=ip,
+                latLng=latlng,
+            ))
+        if latlng is None:
+            resip.append((ip, count))
 
 with open('/usr/local/mad/report/server_map.json') as file:
     json.dump(geoip, file)
