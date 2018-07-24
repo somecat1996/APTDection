968d87ace41af02c4bcdd3248220b7d541b5ccd8
somecat1996 <369150573@qq.com>
2018-07-24 09:58:29 +0800

    .

--- webPage/pages/dist/js/download.js
+++ webPage/pages/dist/js/download.js
@@ -3,6 +3,7 @@
  */
 $(function() {
     $.getJSON("/report/export.json", function (data) {
+        var count = data.length;
         $.each(data, function (i, item) {
             $("#results-display").append("<tr><td>" + item.srcIP +
                 "</td><td>" + item.srcPort +
@@ -10,6 +11,19 @@ $(function() {
                 "</td><td>" + item.dstPort +
                 "</td><td>" + item.UA +
                 "</td><td>" + item.time + "</td></tr>");
+            count--;
+            A();
         });
+        function A() {
+            if (count - 1 == 0) {
+                $('#example').DataTable( {
+                    dom: 'Bfrtip',
+                    buttons: [
+                        'copy', 'csv', 'excel', 'pdf', 'print'
+                    ]
+                } );
+            }
+            else {}
+        }
     });
 });
